b713245ae9f55ed956ee7bc0d7b1e56d4a3e63fd
Roman Kolivashko <rkolivashko@gmail.com>
2021-11-29 23:03:31 -0800

    Added useState hook

--- src/components/NewSongForm.js
+++ src/components/NewSongForm.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NewSongFomr = () => {
+const NewSongForm = ({ addSong }) => {
+  const [title, setTitle] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addSong(title);
+    setTitle('');
+  }
   return ( 
-    
+    <form onSubmit={handleSubmit}>
+      <label>Song name:</label>
+      <input type="text" value={title} required onChange={(e)=> {setTitle(e.target.value)}} />
+      <input type="submit" value="Add song" />
+    </form>
    );
 }
  
-export default NewSongFomr;
+export default NewSongForm;
