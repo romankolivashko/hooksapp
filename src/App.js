604ee6fd88c1f1c7219c960bb6064b9c4af885e9
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-02 12:42:39 -0800

    Added show/close modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import React, { useState } from 'react';
 import Modal from "./components/Modal";
 import SongList from "./components/SongList";
 
 function App() {
+
+  const [showModal, setShowModal] = useState(false);
+
+  console.log(showModal);
+
+  const handleClose = () => {
+    setShowModal(false)
+  }
+
   return (
     <div className="App">
       <SongList />
-      <Modal />
+      {/* <Modal>
+        <h2>10% Off Coupon Code!!</h2>
+        <p>Use Code FREE10 at the checkout</p>
+      </Modal> */}
+      {showModal && <Modal handleClose={handleClose}>
+        <h2>Terms and Conditions</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit necessitatibus autem, et hic corporis sed numquam eos itaque corrupti neque optio adipisci vitae molestiae quaerat. Harum rerum fuga eos! Quae.</p>
+        <a href="#">find out more...</a>
+      </Modal>}
+      <div>
+        <button onClick={() => {setShowModal(true)}}>Show modal</button>
+      </div>
     </div>
   );
 }
