212aa07a960076b627f1a35503fe9d949c876b8c
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-05 10:33:51 -0800

    Added controlled input logic to modal form

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -6,6 +6,10 @@ function NewEventForm() {
   const [date, setDate] = useState('');
 
 
+  const resetForm = () => {
+    setTitle('')
+    setDate('')
+  }
   // const handleChange = (e) => {
   //   console.log(e.target.value);
   //   setTitle(e.target.value);
@@ -16,14 +20,23 @@ function NewEventForm() {
       <form className="new-event-form">
         <label>
           <span>Event Title:</span>
-          <input type="text" onChange={(e) => setTitle(e.target.value)} />
+          <input 
+            type="text" 
+            onChange={(e) => setTitle(e.target.value)} 
+            value={title}
+            />
         </label>
         <label>
           <span>Event Date:</span>
-          <input type="date" onChange={(e) => setDate(e.target.value)} />
+          <input 
+            type="date" 
+            onChange={(e) => setDate(e.target.value)} 
+            value={date}
+            />
         </label>
         <button>Submit</button>
         <p>title - {title}, date- {date} </p>
+        <button onClick={resetForm}>Reset Form</button>
       </form>
     </div>
   )
